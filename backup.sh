4103de3bf0e8d162195e826782ea1475a8cdc6ce
Aureliano <airrloww@gmail.com>
2023-03-16 14:51:30 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,30 +11,19 @@ timestamp=$(date +%Y-%m-%d_%H-%M)
 IFS=':'
 read -a spliter <<< "$target"
 
-# create the destination directory with the timestamp
+    # create the destination directory with the timestamp
+    # find the most recent backup directory
+    # create an incremental backup
+    # create a symlink to the target dir with the newest backup
+
 if [[ $target == *:* ]]; then
     ssh ${spliter[0]} "mkdir -p ${spliter[1]}/$timestamp"
+    latest_backup=$(ssh ${spliter[0]} "ls -td1 ${spliter[1]}/*/ | head -1")
+    rsync -a --link-dest="$latest_backup" $source/ ${spliter[0]}:${spliter[1]}/$timestamp/
+    ssh ${spliter[0]} "ln -sfn ${spliter[1]}/$timestamp ${spliter[1]}/current"
 else
     mkdir -p $target/$timestamp
-fi
-
-# find the most recent backup directory
-if [[ $target == *:* ]]; then
-    latest_backup=$(ssh ${spliter[0]} "ls -td1 ${spliter[1]}/*/ | head -1")
-else
     latest_backup=$(ls -td1 $target/*/ | head -1)
-fi
-
-# create an incremental backup
-if [[ $target == *:* ]]; then
-    rsync -a --link-dest="$latest_backup" $source/ ${spliter[0]}:${spliter[1]}/$timestamp/
-else
     rsync -a --link-dest="$latest_backup" $source/ $target/$timestamp/
-fi
-
-# create a symlink to the target dir with the newest backup
-if [[ $target == *:* ]]; then
-   ssh ${spliter[0]} "ln -sfn ${spliter[1]}/$timestamp ${spliter[1]}/current"
-else
-   ln -sfn $target/$timestamp $target/current
+    ln -sfn $target/$timestamp $target/current 
 fi
